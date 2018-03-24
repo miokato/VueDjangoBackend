d384e4f6e06d0b38cf7d6576f2b8bdc650e9c512
miokato <miokato@me.com>
2018-03-24 11:14:22 +0900

    Connected vue.js

--- VueWithDjangoNew/urls.py
+++ VueWithDjangoNew/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 from blog.urls import router
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(router.urls)),
+    path('api/', include(router.urls)),
+    path('', TemplateView.as_view(template_name='index.html')),
 ]
